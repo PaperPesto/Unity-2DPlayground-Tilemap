e5aaf6463c8da4c2a38839018d57ba11e77e6b5d
Leonardo Bianchini <l.bianchini@aeffegroup.biz>
2019-08-30 14:32:56 +0200

    Frist commit

---
+++ Assets/virtualPosition.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class virtualPosition : MonoBehaviour
+{
+    public int virtual_x { get; set; }
+    public int virtual_y { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Up()
+    {
+        Debug.Log("Up");
+
+        var oldx = transform.position.x;
+        var oldy = transform.position.y;
+
+        transform.position = new Vector2(oldx, oldy + 1);
+    }
+
+    public void Down()
+    {
+        Debug.Log("Down");
+
+        var oldx = transform.position.x;
+        var oldy = transform.position.y;
+
+        transform.position = new Vector2(oldx, oldy - 1);
+    }
+
+    public void Right()
+    {
+        Debug.Log("Right");
+
+        var oldx = transform.position.x;
+        var oldy = transform.position.y;
+
+        transform.position = new Vector2(oldx + 1, oldy);
+    }
+
+    public void Left()
+    {
+        Debug.Log("Left");
+
+        var oldx = transform.position.x;
+        var oldy = transform.position.y;
+
+        transform.position = new Vector2(oldx - 1, oldy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector2(virtual_x, virtual_y);
+    }
+}
